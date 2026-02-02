31976ee29c54c1deb3e9c7492521f5ffd9319510
greg90326 <greg90326@gmail.com>
2026-02-02 15:03:23 +0800

    https://docs.avaloniaui.net/docs/get-started/starter-tutorial/exercises

--- GetStartedApp/GetStartedApp/Views/MainWindow.axaml.cs
+++ GetStartedApp/GetStartedApp/Views/MainWindow.axaml.cs
@@ -11,9 +11,13 @@ namespace GetStartedApp.Views
             InitializeComponent();
         }
 
-        private void Button_OnClick(object? sender, RoutedEventArgs e)
+        private void Celsius_TextChanged(object? sender, RoutedEventArgs e)
         {
-            if (double.TryParse(Celsius.Text, out double C))
+            if (string.IsNullOrEmpty(Celsius.Text) || Celsius.Text == "-")
+            {
+                Fahrenheit.Text = "";
+            }
+            else if (double.TryParse(Celsius.Text, out double C))
             {
                 var F = C * (9d / 5d) + 32;
                 Fahrenheit.Text = F.ToString("0.0");
